cf8b347a1d4116655b37cf9521e10a267638d1e6
Vickixiaodong <xiexiaodong917@163.com>
2017-11-02 14:28:02 +0800

    add get-version demo

---
+++ get-version/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"os"
+)
+
+func main() {
+	fmt.Println("Architecture:", runtime.GOARCH)
+	fmt.Println("Go Version:", runtime.Version())
+	fmt.Println("Operating System:", runtime.GOOS)
+	fmt.Printf("GOPATH=%s\n", os.Getenv("GOPATH"))
+	fmt.Printf("GOROOT=%s\n", runtime.GOROOT())
+}
+
+/*
+Architecture: amd64
+Go Version: go1.9
+Operating System: darwin
+GOPATH=/Users/xiexiaodong/Documents/vagrant/dev-tron/workspace
+GOROOT=/usr/local/go
+ */
