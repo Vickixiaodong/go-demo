ca46f5e80c1138878f84714d30de32cd98598242
Vickixiaodong <xiexiaodong917@163.com>
2017-11-02 20:23:09 +0800

    add db and CLI

--- a-simple-blockchain/main.go
+++ a-simple-blockchain/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"encoding/gob"
+	"flag"
 	"fmt"
+	"github.com/boltdb/bolt"
 	"math"
 	"math/big"
+	"os"
 	"strconv"
 	"time"
 )
 
-const targeBits = 24
+const (
+	targeBits    = 24
+	blocksBucket = "blocksBucket"
+)
 
 type Block struct {
 	Timestamp     int64
@@ -26,6 +33,32 @@ func (b *Block) SetHash() {
 	hash := sha256.Sum256(headers)
 
 	b.Hash = hash[:]
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return &block
 }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
@@ -46,13 +79,42 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 }
 
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
+	var lastHash []byte
+
+	err := bc.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		lastHash = b.Get([]byte("l"))
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	newBlock := NewBlock(data, lastHash)
+
+	err = bc.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		err := b.Put(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		err = b.Put([]byte("l"), newBlock.Hash)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		bc.tip = newBlock.Hash
+
+		return nil
+	})
 }
 
 func NewGenesisBlock() *Block {
@@ -60,19 +122,92 @@ func NewGenesisBlock() *Block {
 }
 
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open("chain-data.db", 0600, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+
+			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := Blockchain{tip, db}
+
+	return &bc
+}
+
+type BlockchainIterator struct {
+	currentHash []byte
+	db          *bolt.DB
+}
+
+func (i *BlockchainIterator) Next() *Block {
+	var block *Block
+
+	err := i.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+		encodedBlock := b.Get(i.currentHash)
+		block = DeserializeBlock(encodedBlock)
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	i.currentHash = block.PrevBlockHash
+
+	return block
+}
+
+func (bc *Blockchain) Iterator() *BlockchainIterator {
+	bci := &BlockchainIterator{bc.tip, bc.db}
+
+	return bci
 }
 
 type ProofOfWork struct {
 	block  *Block
 	target *big.Int
+	isWork bool
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	return pow.isWork
 }
 
 func NewProofOfWork(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-targeBits))
 
-	pow := &ProofOfWork{b, target}
+	pow := &ProofOfWork{b, target, false}
 
 	return pow
 }
@@ -109,6 +244,7 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 		hashInt.SetBytes(hash[:])
 
 		if hashInt.Cmp(pow.target) == -1 {
+			pow.isWork = true
 			break
 		} else {
 			nonce++
@@ -120,43 +256,85 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	return nonce, hash[:]
 }
 
-func main() {
-	bc := NewBlockchain()
+type CLI struct {
+	bc *Blockchain
+}
 
-	bc.AddBlock("Send 1 BTC to sasaxie")
-	bc.AddBlock("Send 2  more BTC to sasaxie")
+func (cli *CLI) Run() {
+	cli.validateArgs()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev Hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Nonce: %x\n", block.Nonce)
-		fmt.Println()
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("data", "", "Block data")
+
+	switch os.Args[1] {
+	case "addblock":
+		err := addBlockCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	case "printchain":
+		err := printChainCmd.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	default:
+		cli.printUsage()
+		os.Exit(1)
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			os.Exit(1)
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
 	}
 }
 
-/*
-Mining the block containint "Genesis Block"
-000000f882b9c702e19abfd62109cdeb15d05bc255698a2d78891d0b6ab840cf
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		fmt.Println("missing cli parameter")
+	}
+}
 
-Mining the block containint "Send 1 BTC to sasaxie"
-00000007dc10cdc3285c3d3c3649cfb5ca6d8e886d1f69689beaee3fd1656800
+func (cli *CLI) addBlock(data string) {
+	cli.bc.AddBlock(data)
+	fmt.Println("Success!")
+}
 
-Mining the block containint "Send 2  more BTC to sasaxie"
-000000e34077705ab88f91905a23558bfce4edb9fd4839783291b893c726490e
+func (cli *CLI) printChain() {
+	bci := cli.bc.Iterator()
 
-Prev Hash:
-Data: Genesis Block
-Hash: 000000f882b9c702e19abfd62109cdeb15d05bc255698a2d78891d0b6ab840cf
-Nonce: f133bb
+	for {
+		block := bci.Next()
 
-Prev Hash: 000000f882b9c702e19abfd62109cdeb15d05bc255698a2d78891d0b6ab840cf
-Data: Send 1 BTC to sasaxie
-Hash: 00000007dc10cdc3285c3d3c3649cfb5ca6d8e886d1f69689beaee3fd1656800
-Nonce: 1ba237
+		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 
-Prev Hash: 00000007dc10cdc3285c3d3c3649cfb5ca6d8e886d1f69689beaee3fd1656800
-Data: Send 2  more BTC to sasaxie
-Hash: 000000e34077705ab88f91905a23558bfce4edb9fd4839783291b893c726490e
-Nonce: f09505
-*/
+		if len(block.PrevBlockHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CLI) printUsage() {
+	fmt.Println("This is CLI.")
+}
+
+func main() {
+	bc := NewBlockchain()
+	defer bc.db.Close()
+
+	cli := CLI{bc}
+	cli.Run()
+}
