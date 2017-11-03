937eeaf43673304eb9c247b768cb02a239f75a89
Vickixiaodong <xiexiaodong917@163.com>
2017-11-03 12:55:31 +0800

    add convert: IntToHex

--- convert/main.go
+++ convert/main.go
@@ -1,1 +1,33 @@
-package convert
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+)
+
+func main() {
+	num := int64(math.MaxInt64)
+	fmt.Println("IntToHex(", num, ") =", IntToHex(num))
+}
+
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+
+	err := binary.Write(buff, binary.BigEndian, num)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("bianry.BigEndian: %#v num: %v\n", binary.BigEndian, num)
+
+	return buff.Bytes()
+}
+
+/*
+bianry.BigEndian: binary.BigEndian num: 9223372036854775807
+IntToHex( 9223372036854775807 ) = [127 255 255 255 255 255 255 255]
+*/
